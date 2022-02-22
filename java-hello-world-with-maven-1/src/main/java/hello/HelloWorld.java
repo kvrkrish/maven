68f440fcbd475b93267f19cdde5781a60d11fd82
RajuK <rajulucky812@gmail.com>
2022-02-22 11:51:00 +0530

    Adding

---
+++ java-hello-world-with-maven-1/src/main/java/hello/HelloWorld.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.joda.time.LocalTime;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+      LocalTime currentTime = new LocalTime();
+		  System.out.println("The current local time is: " + currentTime + "in India");
+
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
